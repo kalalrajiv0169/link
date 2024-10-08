c2a60b2daf2f4bf5f378f74e18fd5b7541b622c8
KALAL RAJIV <153971894+kalalrajiv0169@users.noreply.github.com>
2024-10-08 17:56:37 +0530

    ADD

---
+++ script.js
@@ -0,0 +1,9 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["coding","Developer", "", "Designer", "",""],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
